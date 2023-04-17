f08f777fd6e71f000ace45f40ed383835cfd54a8
FionnKuo <>
2023-04-17 23:30:33 +0800

    Day 29

---
+++ Problem_Solving/Implementation/Service_Lane.py
@@ -0,0 +1,52 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'serviceLane' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY cases
+#
+
+"""
+width 根據實驗數據(index) => 取出最小值(min)
+"""
+
+
+def serviceLane(n, cases):
+    # Write your code here
+    ans = []
+    for j in range(t):
+        x, y = cases[j][0],cases[j][1]
+        ans.append(min(width[x:(y+1)]))
+    return ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    t = int(first_multiple_input[1])
+
+    width = list(map(int, input().rstrip().split()))
+
+    cases = []
+
+    for _ in range(t):
+        cases.append(list(map(int, input().rstrip().split())))
+
+    result = serviceLane(n, cases)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()

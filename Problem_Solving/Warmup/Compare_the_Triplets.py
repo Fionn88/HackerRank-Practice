66a814be7da59dcb4a0aaa6075c1a47f988fa54d
FionnKuo <>
2023-05-03 00:26:12 +0800

    Day 40

---
+++ Problem_Solving/Warmup/Compare_the_Triplets.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def compareTriplets(a, b):
+    ans = []
+    m = 0
+    n = 0
+    for i in range(3):
+        if a[i] > b[i]:
+            m = m + 1
+        elif b[i] > a[i]:
+            n = n + 1
+    ans.append(m)
+    ans.append(n)
+    return ans
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()

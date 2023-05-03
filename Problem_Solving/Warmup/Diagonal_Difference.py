97b1430f38820a9b1965f38acaa3309b2b10356e
FionnKuo <>
2023-05-03 22:26:37 +0800

    Day 41

---
+++ Problem_Solving/Warmup/Diagonal_Difference.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    a = 0
+    b = 0
+
+    
+    for i in range(len(arr)):
+        a = a + arr[i][i]
+        b = b + arr[len(arr) - 1 - i][i]
+
+    return abs(a-b)
+    
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

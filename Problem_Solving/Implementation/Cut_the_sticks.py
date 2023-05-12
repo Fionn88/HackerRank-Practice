c54b00d80d0d7f92469f00e04316217258783971
FionnKuo <>
2023-05-12 10:48:26 +0800

    Day 49

---
+++ Problem_Solving/Implementation/Cut_the_sticks.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'cutTheSticks' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def cutTheSticks(arr):
+    # Write your code here
+    ans = []
+    arr.sort(reverse=True)
+    while arr:
+        ans.append(len(arr))
+        minNum = arr.pop()
+        while arr and minNum == arr[-1]:
+            arr.pop()
+    return ans
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = cutTheSticks(arr)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
